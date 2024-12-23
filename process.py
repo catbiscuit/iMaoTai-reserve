1cda18ff686dae5a3fe6531f33dabb8e2e5003e2
catbiscuit <1606227397@qq.com>
2024-12-23 20:33:03 +0800

    1

--- process.py
+++ process.py
@@ -256,8 +256,8 @@ def reservation(params: dict, mobile: str):
     #     send_msg('！！失败！！茅台预约', f'[{mobile}],登录token失效，需要重新登录')
     #     raise RuntimeError
 
-    msg = f'预约:{desensitize_str(mobile)};Code:{responses.status_code};'
-    logging.info(msg)
+    logging.info(f'预约:mobile:{desensitize_str(mobile)};Code:{response.status_code};')
+    msg = f'预约:{desensitize_str(mobile)};Code:{responses.status_code};Body:{responses.text};'
 
     # 如果是成功，推送消息简化；失败消息则全量推送
     if responses.status_code == 200:
@@ -328,8 +328,7 @@ def getUserEnergyAward(mobile: str):
     response = requests.post('https://h5.moutai519.com.cn/game/isolationPage/getUserEnergyAward', cookies=cookies,
                              headers=headers, json={})
     # response.json().get('message') if '无法领取奖励' in response.text else "领取奖励成功"
-    logging.info(
-        f'领取耐力 : mobile:{desensitize_str(mobile)} :  response code : {response.status_code}')
+    logging.info(f'领取耐力:mobile:{desensitize_str(mobile)};Code:{response.status_code};')
 
 
 # 推送消息
